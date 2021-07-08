0bc8f93920ec7c45285d3b72427071bf363c5386
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-07-08 21:37:56 +0900

    스킬 노드 원형으로 변형 / 알고리즘 수정

--- Assets/01.Scripts/UI/SkillTree/SkillNode.cs
+++ Assets/01.Scripts/UI/SkillTree/SkillNode.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class SkillNode : MonoBehaviour
 {
+    [SerializeField] LineRenderer[] lines;
+    [SerializeField] Transform[] destinations;
+    [SerializeField] NodePos thisPos;
+    [SerializeField] short rootDistance = 0;
+
+    [SerializeField] bool bCanUpgrade = false;
+    public bool BCanUpgrade { get { return bCanUpgrade; } set { bCanUpgrade = value; } }
+
     public enum NodePos
     {
         rightUpper,
@@ -12,11 +20,16 @@ public class SkillNode : MonoBehaviour
         leftLower,
     };
 
-    [SerializeField] NodePos thisPos;
-    [SerializeField] short rootDistance = 0;
-
-    [SerializeField] bool bCanUpgrade = false;
-
-    public bool BCanUpgrade { get { return bCanUpgrade; } set { bCanUpgrade = value; } }
+    private void Update()
+    {
+        for (int i = 0; i < destinations.Length; i++)
+        {
+            if(destinations[i] != null)
+            {
+                lines[i].SetPosition(0, (new Vector3(lines[i].transform.position.x, lines[i].transform.position.y, 1)));
+                lines[i].SetPosition(1, (new Vector3(destinations[i].position.x, destinations[i].position.y, 1)));
+            }
+        }
+    }
 
 }
--- Assets/01.Scripts/UI/SkillTree/SkillTree.cs
+++ Assets/01.Scripts/UI/SkillTree/SkillTree.cs
@@ -18,6 +18,6 @@ public class SkillTree : MonoBehaviour
 
     void UpdateAllNodes()
     {
-
+        
     }
 }
